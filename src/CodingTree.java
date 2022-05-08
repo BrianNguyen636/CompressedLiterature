cbe8491bcb61f6256f480d14d706a69455ef1fcb
Brian Nguyen <briannguyen636@gnail.com>
2022-05-07 17:00:38 -0700

    Lower runtime, fix data loss

--- src/CodingTree.java
+++ src/CodingTree.java
@@ -1,5 +1,7 @@
 import java.util.*;
-
+/*
+@Author Brian Nguyen
+ */
 public class CodingTree {
     Map<Character, String> codes = new HashMap<>();
     byte[] bits;
@@ -7,9 +9,9 @@ public class CodingTree {
     Map<Character, Integer> freqMap;
 
     public CodingTree(String message) {
-        char[] text = message.toCharArray();
         freqMap = new HashMap<>();
-        for (char c : text) {
+        for (int i = 0; i < message.length(); i++) {
+            Character c = message.charAt(i);
             if (!freqMap.containsKey(c)) {
                 freqMap.put(c,1);
             } else {
@@ -21,24 +23,37 @@ public class CodingTree {
 
         mapCodes(codes, queue.peek());
 
-        BitSet bitset = new BitSet(text.length);
-        int i = 0;
-        for (Character c : text) {
+        BitSet bitset = new BitSet();
+        int x = 0;
+        for (int i = 0; i < message.length(); i++) {
+            Character c = message.charAt(i);
+
             String codeString = codes.get(c);
+
             for (int j = 0; j < codeString.length(); j++) {
-                bitset.set(i + j, codeString.charAt(j) != '0');
+                bitset.set(x + j, codeString.charAt(j) != '0');
             }
-            i += codeString.length();
+            x += codeString.length();
         }
-//        String codeString = "";
-//        for (Character c : text) {
-//            codeString += codes.get(c);
-//        }
-//        for (int i = 0; i < codeString.length(); i++) {
-//            bitset.set(i, codeString.charAt(i) != '0');
-//        }
         bits = bitset.toByteArray();
     }
+
+//    String decode(String bits, Map<Character, String> codes) {
+//        String result = "";
+//        Node root = new Node(0);
+//        for (Character c: codes.keySet()) {
+//            String codeString = codes.get(c);
+//            for (int i = 0; i < codeString.length(); i++) {
+//                if (codeString.charAt(i) == '0') {
+//
+//                } else {
+//
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
 
     private void mapCodes(Map<Character,String> map, Node tree) {
         mapCodes(map, tree, "");
@@ -98,6 +113,7 @@ public class CodingTree {
         Node(int theWeight) {
             weight = theWeight;
         }
+        Node(Character theCharacter) {character = theCharacter;}
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.Scanner;
-
+/*
+@Author Brian Nguyen
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +10,7 @@ public class Main {
         testCodingTree();
         long start = System.currentTimeMillis();
         File file = new File("WarAndPeace.txt");
+//        File file = new File(args[0]);
         String text = "";
         long filesize = 0;
         try {
@@ -16,9 +19,15 @@ public class Main {
                 text += scan.nextLine();
             }
             CodingTree tree = new CodingTree(text);
+
+            File codes = new File("codes.txt");
+            OutputStream codeWriter = new FileOutputStream(codes);
+            for (Character c : tree.codes.keySet()) {
+
+            }
+
             File compressed = new File("compressed.txt");
             OutputStream writer = new FileOutputStream(compressed);
-
             writer.write(tree.bits);
             writer.close();
             filesize = compressed.length() / 1024;
@@ -30,12 +39,15 @@ public class Main {
             e.printStackTrace();
         }
         long end = System.currentTimeMillis();
-        System.out.println("Runtime: " + (end - start) / 1000 + " seconds.");
+        System.out.println("Original file size: " + file.length() / 1024 + " kibibytes.");
         System.out.println("Compressed file size: " + filesize + " kibibytes.");
+        System.out.println("Runtime: " + (end - start) / 1000 + " seconds.");
+
+
 
     }
+
     public static void testCodingTree() {
         CodingTree test = new CodingTree("drift tonight");
-        System.out.println(test.bits);
     }
 }
